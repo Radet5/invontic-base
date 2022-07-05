ada4088c40456b2b49a43c8e40130aa77d228f22
Radet 5 <r.5@unbound.services>
2022-07-05 13:56:37 -0400

    move react select config to its own component

--- src/components/modules/grid/record/field/grid-record-field.tsx
+++ src/components/modules/grid/record/field/grid-record-field.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import ReactSelect from "react-select";
+import React, { useEffect, useState, useCallback } from "react";
 
 import "./grid-record-field.scss";
+import { GridSelect } from "./grid-select";
 
 interface GridRecordFieldProps extends FieldInterface {
   onChange: (key: string, value: string | number) => void;
@@ -28,11 +28,6 @@ const GridRecordField = ({
   onKeyUp,
   onKeyDown,
 }: GridRecordFieldProps): JSX.Element => {
-  const [selectedOption, setSelectedOption] = useState<{
-    value: string;
-    label: string;
-  } | null>(null);
-
   const [colWidth, setColWidth] = useState("200px");
 
   useEffect(() => {
@@ -41,121 +36,44 @@ const GridRecordField = ({
     }
   }, [width]);
 
-  useEffect(() => {
-    if (value && options) {
-      setSelectedOption(options.find((option) => option.value === value));
-    }
-  }, [value, options]);
+  const labelElm = (
+    <label
+      className={
+        active
+          ? "m-gridRecordField__label -active"
+          : value
+          ? "m-gridRecordField__label -valid"
+          : "m-gridRecordField__label"
+      }
+      htmlFor={id}
+    >
+      {label}
+    </label>
+  );
 
-  useEffect(() => {
-    if (selectedOption) {
-      onChange(name, selectedOption.value);
-    }
-  }, [selectedOption]);
-
-  const customStyles = {
-    container: (provided, state) => ({
-      ...provided,
-      position: "absolute",
-      width: "100%",
-      bottom: "0",
-    }),
-    control: (provided, state) => ({
-      ...provided,
-      backgroundColor: "#064663",
-      border: "solid 2px #04293A",
-      boxShadow: "none",
-      borderRadius: "0px",
-      padding: "0px",
-      margin: "0px",
-      outline: "none",
-      cursor: "pointer",
-      position: "relative",
-      fontSize: "small",
-      minHeight: "21px",
-      height: "21px",
-    }),
-    singleValue: (provided, state) => ({
-      ...provided,
-      color: "white",
-    }),
-    input: (provided, state) => ({
-      ...provided,
-      color: "white",
-      margin: "0px",
-      padding: "0px",
-    }),
-    valueContainer: (provided, state) => ({
-      ...provided,
-      padding: "0px",
-    }),
-    indicatorsContainer: (provided, state) => ({
-      ...provided,
-      height: "21px",
-      display: "none",
-    }),
-    indicatorSeparator: (provided, state) => ({
-      ...provided,
-      margin: "2px",
-    }),
-    menu: (provided, state) => ({
-      ...provided,
-      backgroundColor: "#064663",
-      fontSize: "small",
-    }),
-    option: (provided, state) => ({
-      ...provided,
-      "&:hover": {
-        backgroundColor: "#04293A",
-      },
-      backgroundColor: state.isFocused ? "#04293A" : null,
-    }),
-  };
   if (type === "reactSelect") {
     return (
-      <div style={{ width: colWidth }} onKeyUp={onKeyUp} className="m-gridRecordField">
-        <ReactSelect
-          value={selectedOption}
+      <div
+        style={{ width: colWidth }}
+        onKeyUp={onKeyUp}
+        className="m-gridRecordField"
+      >
+        <GridSelect
+          value={value}
+          name={name}
+          id={id}
+          onChange={onChange}
+          onFocus={onFocus}
+          fieldRef={fieldRef}
           options={options}
-          onChange={setSelectedOption}
-          ref={fieldRef}
-          styles={customStyles}
-          onFocus={(e) => {
-            e.target.select();
-            onFocus(id);
-          }}
-          menuPosition="fixed"
-          placeholder={null}
         />
-        <label
-          className={
-            active
-              ? "m-gridRecordField__label -active"
-              : value
-              ? "m-gridRecordField__label -valid"
-              : "m-gridRecordField__label"
-          }
-          htmlFor={id}
-        >
-          {label}
-        </label>
+        {labelElm}
       </div>
     );
   } else {
     return (
       <div style={{ width: colWidth }} className="m-gridRecordField">
-        <label
-          className={
-            active
-              ? "m-gridRecordField__label -active"
-              : value
-              ? "m-gridRecordField__label -valid"
-              : "m-gridRecordField__label"
-          }
-          htmlFor={id}
-        >
-          {label}
-        </label>
+        {labelElm}
         <input
           className={
             active
---
+++ src/components/modules/grid/record/field/grid-select.tsx
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from "react";
+import ReactSelect from "react-select";
+
+interface GridSelectProps {
+  id: string;
+  value: string | number;
+  name: string;
+  onChange: (key: string, value: string | number) => void;
+  onFocus: (id: string) => void;
+  fieldRef: any;
+  options: any;
+}
+
+export const GridSelect = ({
+  id,
+  name,
+  value,
+  onChange,
+  onFocus,
+  fieldRef,
+  options,
+}: GridSelectProps): JSX.Element => {
+  const [selectedOption, setSelectedOption] = useState<{
+    value: string;
+    label: string;
+  } | null>(null);
+
+  useEffect(() => {
+    if (value && options) {
+      setSelectedOption(
+        options.find(
+          (option: { value: string; label: string }) => option.value === value
+        )
+      );
+    }
+  }, [value, options]);
+
+  useEffect(() => {
+    if (selectedOption) {
+      onChange(name, selectedOption.value);
+    }
+  }, [selectedOption, name]);
+
+  const customStyles = {
+    container: (provided: any) => ({
+      ...provided,
+      position: "absolute",
+      width: "100%",
+      bottom: "0",
+    }),
+    control: (provided: any) => ({
+      ...provided,
+      backgroundColor: "#064663",
+      border: "solid 2px #04293A",
+      boxShadow: "none",
+      borderRadius: "0px",
+      padding: "0px",
+      margin: "0px",
+      outline: "none",
+      cursor: "pointer",
+      position: "relative",
+      fontSize: "small",
+      minHeight: "21px",
+      height: "21px",
+    }),
+    singleValue: (provided: any) => ({
+      ...provided,
+      color: "white",
+    }),
+    input: (provided: any) => ({
+      ...provided,
+      color: "white",
+      margin: "0px",
+      padding: "0px",
+    }),
+    valueContainer: (provided: any) => ({
+      ...provided,
+      padding: "0px",
+    }),
+    indicatorsContainer: (provided: any) => ({
+      ...provided,
+      height: "21px",
+      display: "none",
+    }),
+    indicatorSeparator: (provided: any) => ({
+      ...provided,
+      margin: "2px",
+    }),
+    menu: (provided: any) => ({
+      ...provided,
+      backgroundColor: "#064663",
+      fontSize: "small",
+    }),
+    option: (provided: any, state: any) => ({
+      ...provided,
+      "&:hover": {
+        backgroundColor: "#04293A",
+      },
+      backgroundColor: state.isFocused ? "#04293A" : null,
+    }),
+  };
+
+  return (
+    <ReactSelect
+      value={selectedOption}
+      options={options}
+      onChange={setSelectedOption}
+      ref={fieldRef}
+      styles={customStyles}
+      onFocus={(e) => {
+        e.target.select();
+        onFocus(id);
+      }}
+      menuPosition="fixed"
+      placeholder={null}
+    />
+  );
+};
