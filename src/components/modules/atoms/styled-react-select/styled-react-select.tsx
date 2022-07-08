3425060e883bc472b3179b902ef372282523bf8a
Radet 5 <r.5@unbound.services>
2022-07-07 22:01:26 -0400

    fix createble dropdown menu issue

--- src/components/modules/atoms/styled-react-select/styled-react-select.tsx
+++ src/components/modules/atoms/styled-react-select/styled-react-select.tsx
@@ -129,7 +129,6 @@ export const StyledReactSelect = ({
         ref={fieldRef}
         styles={customStyles}
         onFocus={onFocus}
-        menuPosition="fixed"
         placeholder={null}
       />
     );
