736e01d69b30294e09db5557e895d54006174bdb
Radet 5 <r.5@unbound.services>
2022-07-15 04:30:58 -0400

    style login component

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { FieldRow } from "../atoms/field-row/field-row";
+import { Field } from "../atoms/field/field";
 
 import "./login.scss";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="login">
-      <div className="login__title">Login</div>
       <div className="login__form">
-        <div className="login__form__input">
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
-        </div>
-        <div className="login__form__input">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
-        </div>
+        <FieldRow noBorder={true}>
+          <Field
+            id={"email"}
+            label="Email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={(name, value) => setEmail(value)}
+          />
+          <Field
+            id={"password"}
+            label="Password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={(name, value) => setPassword(value)}
+          />
+        </FieldRow>
         <div className="login__form__input">
           <button>Login</button>
         </div>
